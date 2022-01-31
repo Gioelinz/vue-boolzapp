4b162c777bbb8c7c9dd3ce6ec7071a12dfc40069
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-31 12:02:55 +0100

    add header username

--- js/script.js
+++ js/script.js
@@ -5,6 +5,27 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
     data: {
-
+        user: {
+            name: 'Gioelinz',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
     }
 })
